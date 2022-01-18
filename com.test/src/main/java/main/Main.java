cc5358aa1db6580bb1d690e4ff51af5d30784f10
Hieu Dang <xuanhieu1989@gmail.com>
2022-01-18 11:26:37 +0700

    change code

--- com.test/src/main/java/main/Main.java
+++ com.test/src/main/java/main/Main.java
@@ -3,8 +3,8 @@ package main;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("hello workd");
+		System.out.println("Hello world!");
+		
 	}
 
 }
